18fc2818ad454ba80885c7b47aa754f1cba420c5
jvjhfhg <jvjhfhg@126.com>
2018-09-22 10:36:39 +0800

    NEW SCANNER

--- 杂项/input-acceleration.cpp
+++ 杂项/input-acceleration.cpp
@@ -1,28 +1,20 @@
-// getchar()读入优化 << 关同步cin << 此优化
-// 用isdigit()会小幅变慢
-// 返回 false 表示读到文件尾
-namespace Reader {
+namespace Scanner {
     const int L = (1 << 15) + 5;
     char buffer[L], *S, *T;
-    __inline bool getchar(char &ch) {
+    
+    __advance __inline char GetChar() {
         if (S == T) {
             T = (S = buffer) + fread(buffer, 1, L, stdin);
-            if (S == T) {
-                ch = EOF;
-                return false;
-            }
+            if (S == T)
+                return -1;
         }
-        ch = *S++;
-        return true;
+        return *S++;
     }
-    __inline bool getint(int &x) {
-        char ch; bool neg = 0;
-        for (; getchar(ch) && (ch < '0' || ch > '9'); ) neg ^= ch == '-';
-        if (ch == EOF) return false;
-        x = ch - '0';
-        for (; getchar(ch), ch >= '0' && ch <= '9'; )
-            x = x * 10 + ch - '0';
-        if (neg) x = -x;
-        return true;
+    
+    template <class Type>
+    __advance __inline void Scan(Type &x) {
+        register char ch; x = 0;
+        for (ch = GetChar(); ~ch && (ch < '0' || ch > '9'); ch = GetChar());
+        for (; ch >= '0' && ch <= '9'; ch = GetChar()) x = x * 10 + ch - '0';
     }
-}
+} using Scanner::Scan;
